7b6443f230a37136b240e15b55d4af80ed998e80
Yoko Fajar Santosa <58869171+yokofajarsantosa@users.noreply.github.com>
2023-02-24 12:59:14 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Peoples</a>
+          <a class="nav-link" href="<?= base_url(); ?>peoples">Peoples</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
